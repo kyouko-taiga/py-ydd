41b8be0a8268c840aaade0e1a33a68f706c0a9a5
Dimitri Racordon <dimitri.racordon@unige.ch>
2015-12-06 13:01:01 +0100

    implement __iter__

--- ydd/ydd.py
+++ ydd/ydd.py
@@ -37,6 +37,33 @@ class YDD(object):
 
         return (not bool(elements)) and (node is self.creator.one)
 
+    def __iter__(self):
+
+        # Implementation note: The iteration process sees the DD as a tree,
+        # and explores all his nodes with a in-order traversal. During this
+        # traversal, we store all the of the "then" parents, so that we can
+        # produce a item whenever we reach the one terminal.
+
+        rv = []
+        stack = []
+        node = self
+
+        while node is not self.creator.zero:
+            if node is self.creator.one:
+                yield frozenset(rv)
+                try:
+                    node = stack.pop()
+                except IndexError:
+                    raise StopIteration()
+                rv = list(filter(lambda e: e < node.key, rv)) + [node.key]
+                node = node.then_
+            elif node.else_ is not self.creator.zero:
+                stack.append(node)
+                node = node.else_
+            else:
+                rv.append(node.key)
+                node = node.then_
+
     def __or__(self, other):
         return self.creator.union(self, other)
 
